8ff716a27e0455ad110c00c232a05080f872304a
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-08-31 01:04:44 +0600

    Modify file: app,js
    Move function: state watcher to view.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
-import onChange from 'on-change';
 import resources from './locales/locales.js';
-import render from './view.js';
+import view from './view.js';
 
 const startApp = () => {
   const state = {
@@ -26,23 +25,11 @@ const startApp = () => {
     feedsContainer: document.querySelector('.container-xxl .row .feeds .card'),
   };
 
-  const stateWatcher = onChange(state, (path, currentValue) => {
-    if (currentValue === '') {
-      return;
-    }
-
-    if (path === 'addingUrlProcess.value') {
-      stateWatcher.addingUrlProcess.state = 'validation';
-    } else if (path === 'addingUrlProcess.state') {
-      render(currentValue, stateWatcher, elements, i18n);
-    }
-  });
-
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const value = formData.get('url');
-    stateWatcher.addingUrlProcess.value = value;
+    view(value, state, elements, i18n);
   });
 };
 
